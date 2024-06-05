74f017374679444e0905b964d763d95e6c8516f4
Hugo-Moreskh <hugo.moreskh@laplateforme.io>
2024-06-05 10:16:10 +0200

    job1 jour2

---
+++ Jour1/job14/index.php
@@ -0,0 +1,34 @@
+<?php
+$str= "Swagg";
+$leat=array("
+  A : 4 ou /-\
+B : 8 ou |3
+C : ( ou [
+D : |)
+E : 3
+F : |=
+G : 6 ou 9
+H : # ou |-|
+I : 1 ou !
+J : _|
+K : |<
+L : 1 ou |_
+M : //\
+N : ||
+O : 0
+P : |*
+Q : 0_
+R : |2 ou |?
+S : 5
+T : 7 ou +
+U : |_|
+V : /
+W : //
+X : ><
+Y : `/ ou ¥
+Z : 2
+");
+function test($str){
+
+}
+echo $leat;
---
+++ Jour2/Job1/index.php
@@ -0,0 +1,23 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>test php</title>
+</head>
+<body>
+<form action="index.php" method="GET">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
+
+
+<?php
+var_dump($_GET)
+?>
