025d155ac7d0ee4676c106638f5589a75a333655
Hugo-Moreskh <hugo.moreskh@laplateforme.io>
2024-06-06 16:24:48 +0200

    jour 3 job 4 re effectué

--- Jour3/Job4/index.php
+++ Jour3/Job4/index.php
@@ -1,24 +1,60 @@
+<!-- Créer un script qui créer un tableau
+ et ajouter une liste de produits.
+ En fonction du prix de celui-ci, une réduction est appliquée.
+ Si un produit a un prix supérieur à 100 € alors une réduction de 10 % est appliquée.
+Chaque produit doit avoir un nom, un prix et une quantité.
+L’ensemble des produits doivent être affiché sur une page HTML. -->
+
+
 <?php
-include 'functions.php';
 
-// Création de la liste des produits
-$products = createProducts();
+session_start();
 
-// Application des réductions sur les produits
-foreach ($products as &$product) {
-    applyDiscount($product);
+if(!isset($_SESSION['listeProduit']) ){
+  $_SESSION['listeProduit']= [];
 }
+array_push($_SESSION['listeProduit'], $_POST);
 
-// Affichage des produits
+
+
+
 ?>
+
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Liste des Produits</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
 </head>
 <body>
-    <h1>Liste des Produits</h1>
-    <?php displayProducts($products); ?>
+  <form action="index.php" method="post">
+    Nom: <input type="text" name="nom"><br>
+    Prix: <input type="number" name="prix"><br>
+    Quantité: <input type="number" name="quantité"><br>
+    <input type="submit" value="Envoyer">
+  </form>
+  <h2>Tableau liste de produits</h2>
+  <table>
+    <tr>
+      <th>Nom:</th>
+      <th>Prix:</th>
+      <th>Quantité:</th>
+      <th>Total:</th>
+    </tr>
+<?php
+ foreach ($_SESSION['listeProduit'] as $value) {
+  $total= $value['prix'] * $value['quantité'];
+  if ($total>100){
+    $total= $total * 0.9;
+  }
+  echo "<tr>";
+  echo "<td>" . htmlspecialchars($value['nom']) . "</td>";
+  echo "<td>" . htmlspecialchars($value['prix']) . "</td>";
+  echo "<td>" . htmlspecialchars($value['quantité']) . "</td>";
+  echo "<td>" . htmlspecialchars($total) . "</td>";
+  echo "</tr>";
+}
+?>
 </body>
 </html>
