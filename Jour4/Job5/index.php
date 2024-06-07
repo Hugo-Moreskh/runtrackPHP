b060185d3736628963dffb180331333c5ab14d15
Hugo-Moreskh <hugo.moreskh@laplateforme.io>
2024-06-07 16:04:50 +0200

    job 1 et 2 jour 05

---
+++ Jour4/Job5/index.php
@@ -0,0 +1,13 @@
+<!-- Développez le fameux jeu du morpion.
+ Faites un tableau HTML avec 3 ligneset 3 colonnes représentant la grille.
+  Au début de la partie, chacune des casescontient un bouton de type submit dont la valeur est “-”.
+   Si un joueur cliquesur ce bouton, le bouton est remplacé par un “O” ou par un “X”.
+    C’est le joueur“X” qui commence.
+Dès qu’un joueur a gagné, affichez “X a gagné” ou “O a gagné”
+ et réinitialisezla partie.
+  Si toutes les cases ont été cliquées et que personne n’a gagné,
+affichez “Match nul” et réinitialisez la partie.
+Un bouton “réinitialiser la partie”présent en dessous de la grille
+ permet également de réinitialiser la partie à
+tout moment. -->
+
